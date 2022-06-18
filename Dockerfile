c1f2d18d49655fcc3b4e678c9374b2b68e5a0e2a
Christian Benavidez <christianbenavidez57@gmail.com>
2022-06-18 14:10:29 -0700

    feat: initialize repo with dummy files
    Initialize the repo with a basic Dockerfile and .dockerignore. Just
    add in a README even if its empty. Add in the go module files and the
    main file along with a test file for it.
    
    This should be a decent place to start from.

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM golang:1.18.3-bullseye
+
+WORKDIR /usr/src/app
+
+COPY go.mod go.sum ./
+RUN go mod download && go mod verify
+
+COPY . .
+RUN go test
+RUN go build -v -o /usr/local/bin/app ./...
+
+CMD ["app"]
---
+++ main_test.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+  "testing"
+  "github.com/stretchr/testify/assert"
+)
+
+func TestHelloWorld(t *testing.T) {
+  assert.Equal(t, "Hello, World!", "Hello, World!", "They should be equal.")
+}
